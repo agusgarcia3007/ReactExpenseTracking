a65c48846c9365fb8161ba3d24d29ddd9ecd8297
agusgarcia3007 <agusgarcia3007@gmail.com>
2021-10-19 10:04:31 -0300

    everything working except for the unique id generator

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,14 +1,11 @@
-import React, { useReducer } from 'react';
-
+import React, { useState, useReducer } from 'react';
+import Error from './Error';
+import shortid from 'shortid';
 
 const Form = () => {
 
-    
-
-    const initialState = {
-        Expense : '',
-        price:0
-    }
+     
+    const [error, setError] = useState(false);
 
     const formReducer = (state, {type, payload})=>{
         switch(type){
@@ -16,18 +13,38 @@ const Form = () => {
                 return{...state, Expense: payload}
             case 'price':
                 return{...state, price: payload}
+            case 'id':
+                return{...state, id : payload}
             default:
                 throw new Error();
         }
     }
 
-    const [state, dispatch] = useReducer(formReducer, initialState);
+    
+    const [state, dispatch] = useReducer(formReducer, {
+        Expense : '',
+        price:0,
+        id:shortid.generate()
+    });
+    
+   
+    
 
     //when the user add an expense
     const addExpense = e => {
         e.preventDefault();
 
+        //validation
+        if(state.price < 1 || isNaN(state.price) || state.Expense.trim() === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
+
+        //add an expense
         
+        console.log(state);
+
     }
 
     return(
@@ -35,6 +52,9 @@ const Form = () => {
         onSubmit={addExpense}
     >
         <h2>Did you spend some money?</h2>
+
+        {error ? <Error message ='Both fields are Required'/> : null}
+
         <div className="field">
             <label>Expense</label>
             <input type="text"
